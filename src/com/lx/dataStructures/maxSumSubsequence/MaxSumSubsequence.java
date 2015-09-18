d9ea14c0cc3932d7e5b2b89729d0282d3f5d546e
Octivian <408199559@qq.com>
2015-09-18 18:51:38 +0800

    modify

--- src/com/lx/dataStructures/maxSumSubsequence/MaxSumSubsequence.java
+++ src/com/lx/dataStructures/maxSumSubsequence/MaxSumSubsequence.java
@@ -54,24 +54,17 @@ public class MaxSumSubsequence {
 	
 	
 	/**
-	 * 算法3
+	 * 算法3（分治）
 	 * 时间算法复杂度  O(n^2)
 	 * @param a
 	 * @return
 	 */
 	private static long sum3(long[] a){
-		long maxSum=0;
-		for(int i = 0;i<a.length;i++){//循环起点
-			long thisSum = 0;
-			for(int j = i;j<a.length;j++){//循环终点
-				thisSum+=a[j];
-				System.out.println(thisSum);
-				if(thisSum>maxSum){
-					maxSum = thisSum;
-				}
-			}
-		}
-		return maxSum;
-	}
+		long firstHalf=0;//前半部分最大子序列
+		long lastHalf=0;//后半部分最大子序列
+		long firstHalfWithLastElement=0;//前半部分包含前半部分最后一个元素的最大子序列
+		long lastHalfWithFirstElement=0;//后半部分包含后半部分第一个元素的最大子序列
+		return firstHalf;
+	}   
 
 }
