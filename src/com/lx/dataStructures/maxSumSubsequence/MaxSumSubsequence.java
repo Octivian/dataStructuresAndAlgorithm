077c2bef0138a418a574a9332f5534389c3fcc97
Octivian <408199559@qq.com>
2015-09-21 18:32:18 +0800

    recursive

--- src/com/lx/dataStructures/maxSumSubsequence/MaxSumSubsequence.java
+++ src/com/lx/dataStructures/maxSumSubsequence/MaxSumSubsequence.java
@@ -4,7 +4,8 @@ package com.lx.dataStructures.maxSumSubsequence;
 public class MaxSumSubsequence {
 
 	public static void main(String[] args) {
-		System.out.println(sum2(new long[]{-2, 11, -4, 13, -5, -2}));
+		System.out.println(sum3(new long[]{-2, 11, -4, 13, -5, -2},0,5));
+//		System.out.println(sum2(new long[]{-2, 11, -4, 13, -5, -2}));
 	}
 	
 	/**
@@ -43,7 +44,6 @@ public class MaxSumSubsequence {
 			long thisSum = 0;
 			for(int j = i;j<a.length;j++){//循环终点
 				thisSum+=a[j];
-				System.out.println(thisSum);
 				if(thisSum>maxSum){
 					maxSum = thisSum;
 				}
@@ -59,12 +59,55 @@ public class MaxSumSubsequence {
 	 * @param a
 	 * @return
 	 */
-	private static long sum3(long[] a){
-		long firstHalf=0;//前半部分最大子序列
-		long lastHalf=0;//后半部分最大子序列
+	private static long sum3(long[] a,int left,int right){
+		System.out.println("left:"+left+",right:"+right);
+		if(left == right){
+			if(a[left]>0){
+				return a[left];
+			}else{
+				return 0;
+			}
+		}
+		int center = (left + right)/2;
+		System.out.println("center: "+center);
+		//前半部分最大子序列
+		long firstHalf=sum3(a,left,center);
+		System.out.println("firstHalf:"+firstHalf);
+		//后半部分最大子序列
+		long lastHalf=sum3(a,center+1,right);
+		System.out.println("lastHalf:"+lastHalf);
 		long firstHalfWithLastElement=0;//前半部分包含前半部分最后一个元素的最大子序列
+		long tem1 = 0;
+		for(int i = center;i>=left;i--){
+			tem1+=a[i];
+			if(tem1>firstHalfWithLastElement){
+				firstHalfWithLastElement = tem1;
+			}
+		}
 		long lastHalfWithFirstElement=0;//后半部分包含后半部分第一个元素的最大子序列
-		return firstHalf;
+		long tem2 = 0;
+		for(int i = center+1;i<=right;i++){
+			tem2+=a[i];
+			if(tem2>lastHalfWithFirstElement){
+				lastHalfWithFirstElement = tem2;
+			}
+		}
+		long combine = lastHalfWithFirstElement+firstHalfWithLastElement;
+		System.out.println("combine:"+combine);
+		
+		if(firstHalf>lastHalf){
+			if(firstHalf>combine){
+				return firstHalf;
+			}else{
+				return combine;
+			}
+		}else{
+			if(lastHalf>combine){
+				return lastHalf;
+			}else{
+				return combine;
+			}
+		}
 	}   
 
 }
