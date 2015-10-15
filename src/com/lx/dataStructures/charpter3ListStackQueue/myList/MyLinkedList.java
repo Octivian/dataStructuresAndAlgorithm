77f960bdfdc042eab964fe8338319f89df8a698c
Octivian <408199559@qq.com>
2015-10-15 14:33:57 +0800

    MyLinkedList

--- src/com/lx/dataStructures/charpter3ListStackQueue/myList/MyLinkedList.java
+++ src/com/lx/dataStructures/charpter3ListStackQueue/myList/MyLinkedList.java
@@ -1,5 +1,9 @@
 package com.lx.dataStructures.charpter3ListStackQueue.myList;
 
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 
 /**
  * 1.包含两端的链，表的大小以及一些方法
@@ -35,18 +39,114 @@ public class MyLinkedList {
 		return size() == 0;
 	}
 	
-	private void addBefore(Node<Object> p,Object x){
-		
-	} 
+	public void add(Object x){
+		addBefore(getNode(size()), x);
+	}
+	
+	public void add(int idx,Object x){
+		addBefore(getNode(idx), x);
+	}
+	public Object get(int idx){
+		return getNode(idx).data;
+	}
+	
+	public Object set(int idx,Object x){
+		Node<Object> currentNode = getNode(idx);
+		Object oldX = currentNode.data;
+		currentNode.data = x;
+		return oldX;
+	}
+	
+	public Object remove(int idx){
+		return remove(getNode(idx));
+	}
+	
+	private Object remove(Node<Object> n){
+		n.prev.next = n.next;
+		n.next.prev = n.prev;
+		size--;
+		modCount++;
+		return n.data;  
+	}
+	
+	private Node<Object> getNode(int idx){
+		if(idx > size||idx<0){
+			throw new IndexOutOfBoundsException();
+		}
+		Node<Object> n;
+		if(idx < size()/2){
+			n = beginMarker.next;
+			for(int i = 0;i<idx;i++){
+				n = n.next;
+			}
+		}else{
+			n = endMarker;
+			for(int i = size();i>idx;i--){
+				n=n.prev;
+			}
+		}
+		return n;
+	}
+	
+	private void addBefore(Node<Object> n,Object x){
+		Node<Object> node = new Node<Object>(x,n.prev,n);
+		n.prev = node;
+		node.prev.next = node;
+		size++;
+		modCount++;
+	}
 	
 	private static class Node<Object>{
 		public Object data;
 		public Node<Object> prev;
 		public Node<Object> next;
-		public Node(Object x,Node<Object> previous,Node<Object> n) {
+		public Node(Object x,Node<Object> p,Node<Object> n) {
 			data = x;
-			prev = previous;
+			prev = p;
 			next = n;
 		}
+	}
+	
+	public Iterator<Object> iterator(){
+		return new LinkedListIterator();
+	}
+	
+	private class LinkedListIterator implements Iterator<Object>{
+
+		private  Node<Object> current = beginMarker;
+		private  int expectedModCount = modCount;
+		private  boolean okToRemove = false;
+		
+		@Override
+		public boolean hasNext() {
+			return current!=endMarker;
+		}
+
+		@Override
+		public Object next() {
+			if(modCount!=expectedModCount){
+				throw new ConcurrentModificationException();
+			}
+			if(!hasNext()){
+				throw new NoSuchElementException();
+			}
+			current = current.next;
+			okToRemove = true;
+			return current.data;
+		}
+
+		@Override
+		public void remove() {
+			if(modCount!=expectedModCount){
+				throw new ConcurrentModificationException();
+			}
+			if(!okToRemove){
+				throw new IllegalStateException();
+			}
+			MyLinkedList.this.remove(current.prev);
+			okToRemove = false;
+			expectedModCount++;
+		}
+		
 	}
 }
