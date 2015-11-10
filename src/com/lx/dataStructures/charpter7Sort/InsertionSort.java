c43a857d265f7725fcf7eb5e78c936e82b9c18c4
Octivian <408199559@qq.com>
2015-11-10 23:13:32 +0800

    fix inner loop index bug

--- src/com/lx/dataStructures/charpter7Sort/InsertionSort.java
+++ src/com/lx/dataStructures/charpter7Sort/InsertionSort.java
@@ -1,13 +1,20 @@
 package com.lx.dataStructures.charpter7Sort;
 
 public class InsertionSort {
+	
+	public static void main(String[] args) {
+		Integer[] a =  {81,94,11,96,12,35,17,95,28,58,41,75,15};
+		insertionSort(a);
+		for(Integer num:a)
+		System.out.print(num+",");
+	}
 
 	public static <AnyType extends Comparable<? super AnyType>> void insertionSort(AnyType[] a){
 		int j;
 		for (int i = 1; i < a.length; i++) {
 			AnyType temp = a[i];
-			for (j = 0; j < i&&temp.compareTo(a[j])<0; j++) //一小皆小
-				a[j+1]=a[j];
+			for (j = i; j >0&&temp.compareTo(a[j-1])<0; j--) //一小皆小
+				a[j]=a[j-1];
 			a[j] = temp;
 		}
 	}
